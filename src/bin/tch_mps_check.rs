e0e7b4cd33865a075353456e4bb7b24eadf6b129
nickverneck <nicollas@gmail.com>
2026-01-04 22:54:11 -0500

    refactor: conditionally select MPS or CPU device based on availability

--- src/bin/tch_mps_check.rs
+++ src/bin/tch_mps_check.rs
@@ -5,9 +5,6 @@ fn main() {
     println!("cuda available: {}", tch::Cuda::is_available());
     println!("cudnn available: {}", tch::Cuda::cudnn_is_available());
 
-    let device = Device::default();
-    println!("default device: {:?}", device);
-
     // MPS support is exposed via Device::Mps in tch when libtorch has MPS.
     let mps_device = Device::Mps;
     let mps_available = std::panic::catch_unwind(|| {
@@ -15,4 +12,7 @@ fn main() {
     })
     .is_ok();
     println!("mps device usable: {}", mps_available);
+
+    let device = if mps_available { Device::Mps } else { Device::Cpu };
+    println!("selected device: {:?}", device);
 }
