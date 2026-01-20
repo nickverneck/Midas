80c07068bf94d8a35ac19bc20b123a6413e95982
nickverneck <nicollas@gmail.com>
2026-01-20 13:49:03 -0500

    fixed thc check

--- src/bin/tch_mps_check.rs
+++ src/bin/tch_mps_check.rs
@@ -4,6 +4,13 @@ fn main() {
     println!("tch crate version: {}", env!("CARGO_PKG_VERSION"));
     println!("cuda available: {}", tch::Cuda::is_available());
     println!("cudnn available: {}", tch::Cuda::cudnn_is_available());
+    if tch::Cuda::is_available() {
+        println!("tch cuda device count: {}", tch::Cuda::device_count());
+        if tch::Cuda::device_count() > 0 {
+            println!("tch cuda device 0 name: {}", tch::Cuda::device_name(0).unwrap_or_else(|_| "unknown".to_string()));
+        }
+    }
+    println!("tch cuda version: {}", tch::Cuda::version());
 
     // MPS support is exposed via Device::Mps in tch when libtorch has MPS.
     let mps_device = Device::Mps;
